ddde269fb567634aa12cd515c68d446c00be9800
Jeanette Schwarz <jeanne.schwarz1306@gmail.com>
2024-02-06 11:41:07 +0100

    fixes

--- React/react-with-arrays_fruits/src/components/Card.js
+++ React/react-with-arrays_fruits/src/components/Card.js
@@ -2,9 +2,8 @@ import "./Card.css";
 
 export default function Card({ name, color }) {
   return (
-    <p className="card">
+    <p className="card" style={{ backgroundColor: color }}>
       {name}
-      {color}
     </p>
   );
 }
