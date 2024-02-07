15ec1c2037e1795a5e00bcbe06e5894fedc6e3bc
Jeanette Schwarz <jeanne.schwarz1306@gmail.com>
2024-02-07 16:31:00 +0100

    pokemon with paging

---
+++ React/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+
+export default function PokemonList() {
+  const [pokemon, setPokemon] = useState([]);
+  const [pokemonId, setpokemonId] = useState(0);
+
+  useEffect(() => {
+    async function loadPokemon() {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon?offset=${pokemonId}`
+        );
+        const data = await response.json();
+        setPokemon(data.results);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    loadPokemon();
+  }, [pokemonId]);
+
+  if (!pokemon) {
+    return null;
+  }
+
+  return (
+    <main>
+      <button
+        type="button"
+        onClick={() => {
+          setpokemonId((pokemonId) => pokemonId - 1);
+        }}
+        disabled={pokemonId === 0}
+      >
+        Previous Page
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          setpokemonId((pokemonId) => pokemonId + 1);
+        }}
+      >
+        Next Page
+      </button>
+      <ul>
+        {pokemon.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+    </main>
+  );
+}
