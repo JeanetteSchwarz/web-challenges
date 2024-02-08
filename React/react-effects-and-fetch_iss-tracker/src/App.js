ee959bbaa36308778e79df1fbe0e2f6cc67b82ed
Jeanette Schwarz <jeanne.schwarz1306@gmail.com>
2024-02-08 11:08:01 +0100

    iss

---
+++ React/react-effects-and-fetch_iss-tracker/src/App.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import Controls from "./components/Controls";
+import Map from "./components/Map";
+import "./styles.css";
+
+//const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+export default function App() {
+  const [coords, setCoords] = useState({
+    longitude: 0,
+    latitude: 0,
+    id: 0,
+  });
+  const [interval, setInterval] = useState();
+
+  async function getISSCoords() {
+    const response = await fetch(
+      "https://api.wheretheiss.at/v1/satellites/25544"
+    );
+    const coords = await response.json();
+    console.log(coords);
+
+    setCoords(coords);
+  }
+
+  // setInterval to refresh automatically every five seconds
+
+  useEffect(() => {
+    //const intervalID = coords.id;
+    setInterval(getISSCoords, 5000);
+
+    /* return () => {
+      clearInterval(interval);
+    }; */
+  }, []);
+
+  return (
+    <main>
+      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Controls
+        longitude={coords.longitude}
+        latitude={coords.latitude}
+        onRefresh={getISSCoords}
+      />
+    </main>
+  );
+}
