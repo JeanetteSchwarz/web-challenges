beb26f518220a9043aae55486c76d0dd475d374c
Jeanette Schwarz <jeanne.schwarz1306@gmail.com>
2024-02-08 15:23:05 +0100

    new

---
+++ React/react-with-local-storage_dice/src/App.js
@@ -0,0 +1,40 @@
+import { D6Button } from "./components/D6Button";
+import History from "./components/History";
+import "./styles.css";
+import useLocalStorageState from "useLocalStorageState";
+
+export default function App() {
+  const [rolls, setRolls] = useLocalStorageState("rolls", { defaultValue: [] });
+
+  return (
+    <div className="app">
+      <main className="app__main">
+        <D6Button rolls={rolls} setRolls={setRolls} />
+      </main>
+      <aside className="app__aside">
+        <History rolls={rolls} />
+      </aside>
+    </div>
+  );
+}
+
+/* import useLocalStorageState from "use-local-storage-state";
+//import { useState } from "react";
+import { D6Button } from "./components/D6Button";
+import History from "./components/History";
+import "./styles.css";
+
+export default function App() {
+  const [rolls, setRolls] = useLocalStorageState("key", { defaultValue: [] });
+  return (
+    <div className="app">
+      <main className="app__main">
+        <D6Button rolls={rolls} setRolls={setRolls} />
+      </main>
+      <aside className="app__aside">
+        {/* <History rolls={[]} /> }
+        <History rolls={rolls} />
+      </aside>
+    </div>
+  );
+} */
