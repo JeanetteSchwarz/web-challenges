567bfd9909ed573f75a98e007d4d303b4ff5dd53
Jeanette Schwarz <jeanne.schwarz1306@gmail.com>
2024-02-20 22:50:34 +0100

    random character

--- Backend/backend-api-routes_random-character/pages/index.js
+++ Backend/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,23 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data } = useSWR(`/api/random-character`, fetcher);
+  console.log(data);
+
+  return (
+    <>
+      <h1>Hello from the other side...</h1>
+      <div>
+        {data.firstName}
+        <br></br>
+        {data.lastName}
+        <br></br>
+        {data.twitterName}
+        <br></br>
+        {data.geohash}
+      </div>
+    </>
+  );
 }
