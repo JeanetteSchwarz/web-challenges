b943634c0bd8d8caedbc42ee3f9597d9d91ec853
Jeanette Schwarz <jeanne.schwarz1306@gmail.com>
2024-02-20 11:08:52 +0100

    Backend

---
+++ Backend/backend-basics_routes/server.js
@@ -0,0 +1,14 @@
+import { createServer } from "node:http";
+
+export const server = createServer((request, response) => {
+  if (request.url === "/api/fish/1") {
+    response.statusCode = 200;
+    response.end("Shrimp 🦐");
+  } else if (request.url === "/api/fish/2") {
+    response.statusCode = 200;
+    response.end("Anemonefish 🐠");
+  } else {
+    response.statusCode = 404;
+    response.end("Not found");
+  }
+});
